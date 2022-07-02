be577c4cd13cfd594ed63a0ae2ad1bdb131dd375
a-korkin <a-korkin88@yandex.ru>
2022-07-02 16:17:46 +0300

    feat: infrastructure project added

---
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Application.Interfaces;
+using Domain.Entities.Admin;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+}
